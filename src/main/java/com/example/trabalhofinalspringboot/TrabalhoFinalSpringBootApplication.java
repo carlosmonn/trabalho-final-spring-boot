d307031bc2fee653daa496af633ee216b31f4712
carlosmonn <karlinhos_monn@hotmail.com>
2018-12-11 21:16:49 -0200

    Primeiro commit

---
+++ src/main/java/com/example/trabalhofinalspringboot/TrabalhoFinalSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.trabalhofinalspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoFinalSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoFinalSpringBootApplication.class, args);
+	}
+}
