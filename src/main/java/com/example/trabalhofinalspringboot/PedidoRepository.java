9b9e251d14dfcd6199eea8e75b9b5cab144e02a2
carlosmonn <karlinhos_monn@hotmail.com>
2018-12-19 19:22:14 -0200

    Pesquisa de pedidos pela data de criação

--- src/main/java/com/example/trabalhofinalspringboot/PedidoRepository.java
+++ src/main/java/com/example/trabalhofinalspringboot/PedidoRepository.java
@@ -1,9 +1,17 @@
 package com.example.trabalhofinalspringboot;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "pedidos")
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-
+	
+	@RestResource(path = "/searchDataCriacao")
+	@Query(value = "SELECT * FROM pedido WHERE FORMATDATETIME(DATA_CRIACAO, 'yyyy-MM-dd') = :datacriacao", nativeQuery = true)
+	List<Pedido> findByDataCriacao(@Param("datacriacao") String dataCriacao);
 }
